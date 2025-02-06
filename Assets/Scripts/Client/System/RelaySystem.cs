2fbf20c8d547665d03762d06a05f70a1aec3ce16
Koesob <epi3706@khu.ac.kr>
2025-02-06 21:51:41 +0900

    Lobby - Relay 연결 기능 구현 완

---
+++ Assets/Scripts/Client/System/RelaySystem.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+namespace CCGP.Client
+{
+    public static class RelaySystem
+    {
+        public struct RelayHostData
+        {
+            public string JoinCode;
+            public string IPv4Address;
+            public ushort Port;
+            public Guid AllocationId;
+            public byte[] AllocationIdBytes;
+            public byte[] ConnectionData;
+            public byte[] Key;
+        }
+
+        public struct RelayJoinData
+        {
+            public string IPv4Address;
+            public ushort Port;
+            public Guid AllocationId;
+            public byte[] AllocationIdBytes;
+            public byte[] ConnectionData;
+            public byte[] HostConnectionData;
+            public byte[] Key;
+        }
+
+        public static async Task<RelayHostData> SetupRelay(int maxConnections)
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+
+            RelayHostData data = new RelayHostData
+            {
+                JoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId),
+                IPv4Address = allocation.RelayServer.IpV4,
+                Port = (ushort)allocation.RelayServer.Port,
+                AllocationId = allocation.AllocationId,
+                AllocationIdBytes = allocation.AllocationIdBytes,
+                ConnectionData = allocation.ConnectionData,
+                Key = allocation.Key
+            };
+
+            return data;
+        }
+
+        public static async Task<RelayJoinData> JoinRelay(string joinCode)
+        {
+            JoinAllocation allocation;
+
+            try
+            {
+                allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            }
+            catch (RelayServiceException e)
+            {
+                Debug.LogError($"RelayServiceException during JoinRelay: {e.Message}");
+                return default;
+            }
+
+            return new RelayJoinData
+            {
+                IPv4Address = allocation.RelayServer.IpV4,
+                Port = (ushort)allocation.RelayServer.Port,
+                AllocationId = allocation.AllocationId,
+                AllocationIdBytes = allocation.AllocationIdBytes,
+                ConnectionData = allocation.ConnectionData,
+                HostConnectionData = allocation.HostConnectionData,
+                Key = allocation.Key
+            };
+        }
+    }
+}
---
+++ Assets/Scripts/Client/UI/Lobby/Content_LobbyEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CCGP.Client
+{
+    public class Content_LobbyEntry : MonoBehaviour
+    {
+        public TMP_Text lobbyNameText;    // 로비 이름 표시용 텍스트
+        public TMP_Text playerCountText;  // 플레이어 수 표시용 텍스트
+        public Button joinButton;     // 로비 참가 버튼
+
+        private Lobby lobbyData;
+        private Action<Lobby> onJoinCallback;
+
+        // 로비 데이터와 콜백을 설정하여 UI 초기화
+        public void Setup(Lobby lobby, Action<Lobby> joinCallback)
+        {
+            lobbyData = lobby;
+            lobbyNameText.text = lobby.Name;
+            playerCountText.text = $"{lobby.Players.Count}/{lobby.MaxPlayers}";
+            onJoinCallback = joinCallback;
+            joinButton.onClick.RemoveAllListeners();
+            joinButton.onClick.AddListener(OnJoinButtonClicked);
+        }
+
+        private void OnJoinButtonClicked()
+        {
+            onJoinCallback?.Invoke(lobbyData);
+        }
+    }
+}
